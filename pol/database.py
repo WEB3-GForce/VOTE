52b44b644bbdd82a57f76040063ee6903e90696d
WEB3-GForce <webiii@localhost>
2015-12-16 18:57:15 -0500

    Updating Database

---
+++ pol/database.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+# MongoClient and database
+CLIENT = MongoClient()
+DB = CLIENT.vote
+
+# Collections for all the objects
+MEMBER = DB.member
+GROUP  = DB.group
+BILL   = DB.bill
+ISSUE  = DB.issue
