2c089d3730c2266f855be3135e0e59e8aa5d98af
WEB3-GForce <webiii@localhost>
2015-12-16 20:41:03 -0500

    Altering load_data

--- dumps/load_data.py
+++ dumps/load_data.py
@@ -8,8 +8,10 @@ for mdata in open("member.txt").read().splitlines():
     members.append(loads(mdata))
 
 for member_data in members:
-    if not db.MEMBER.find(member_data):
-        db.MEMBER.insert(members)
+    if not db.MEMBER.find({"_id": member_data["_id"]}):
+        db.MEMBER.insert(member_data)
+    else:
+        db.MEMBER.update({"_id": member_data["_id"]}, member_data)
 
 # import groups
 groups = []
@@ -17,5 +19,7 @@ for gdata in open("group.txt").read().splitlines():
     groups.append(loads(gdata))
 
 for group_data in groups:
-    if not db.GROUP.find(group_data):
-        db.GROUP.insert(groups)
+    if not db.GROUP.find({"_id": group_data["_id"]}):
+        db.GROUP.insert(group_data)
+    else:
+        db.GROUP.update({"_id": group_data["_id"]}, group_data)
