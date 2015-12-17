276fa3bb2af2fe0bd784d46ba2f693a583acc92d
WEB3-GForce <webiii@localhost>
2015-12-16 20:44:10 -0500

    Adding rest of collections

--- pol/database.py
+++ pol/database.py
@@ -5,8 +5,15 @@ CLIENT = MongoClient()
 DB = CLIENT.vote
 
 # Collections for all the objects
-MEMBER = DB.member
-GROUP  = DB.group
-BILL   = DB.bill
-ISSUE  = DB.issue
+MEMBER   = DB.member
+GROUP    = DB.group
+BILL     = DB.bill
+ISSUE    = DB.issue
 RELATION = DB.relation
+STANCE   = DB.stance
+STRATEGY = DB.strategy
+DECISION = DB.decision
+
+# collection list
+
+collection_list = [MEMBER, GROUP, BILL, ISSUE, RELATION, STANCE, STRATEGY, DECISION]
