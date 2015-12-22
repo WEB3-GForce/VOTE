9532fc9b18991635837a369ddef78a82fd582a6d
Krishnan <krishnan.srinivasan@yale.edu>
2015-12-22 12:31:27 -0800

    load_data now loads stance objects in issue

--- dumps/load_data.py
+++ dumps/load_data.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from bson.json_util import loads
 import pol.database as db
+from pol.stance import Stance
 
 for COLLECTION in db.collection_list:
     result = []
@@ -8,6 +9,9 @@ for COLLECTION in db.collection_list:
         result.append(loads(data))
 
     for hash_data in result:
+        if COLLECTION.name == 'issue':
+            if "norm" in hash_data and hash_data["norm"]:
+                hash_data["norm"] = db.encode_classes(Stance(**hash_data["norm"]))
         if not COLLECTION.find_one({"_id": hash_data["_id"]}):
             COLLECTION.insert(hash_data)
         else:
