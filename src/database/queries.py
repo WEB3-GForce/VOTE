18e23954c8a68238f36df821771ddf24a6df8d6e
WEB3-GForce <william.bailey.333@gmail.com>
2016-04-01 19:06:11 -0400

    Adding queries

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,4 +1,23 @@
+"""
+    VOTE - A decision program for predicting votes in Congress.
+    Copyright (C) 2016 William Edward Bailey, III (WEB3 or WEBIII):
+      https://github.com/WEB3-GForce
+    Based on Stephen Slade's Ph.D Thesis:
+      zoo.cs.yale.edu/classes/cs458/materials/RealisticRationality.pdf
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 def bill_query(bill_identifier):
     return {"$or": [{"name": bill_identifier},
@@ -10,3 +29,10 @@ def group_query(group_identifier):
     return  {"$or": [{"name": group_identifier},
                      {"synonyms": { "$in" : [ group_identifier ]}},
                      {"_id" : group_identifier}] }
+
+def issue_query(issue_identifier):
+    return  {"$or": [{"name": issue_identifier},
+                     {"synonyms": { "$in" : [ issue_identifier ] }},
+                     {"_id" : issue_identifier}
+                    ]
+            }
