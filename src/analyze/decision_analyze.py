36f2b0c42746b79f2b9720e5da38e0566e635bba
WEB3-GForce <william.bailey.333@gmail.com>
2016-04-13 11:46:53 -0400

    Adding more logging to functions

--- src/analyze/decision_analyze.py
+++ src/analyze/decision_analyze.py
@@ -41,9 +41,10 @@ def update_decision_metrics(decision):
     and determines which side the different parties (group, member, bill)
     would support.    
     """
+    logger.LOGGER.info("Updating decision metrics...")
     _update_regular_stances(decision)
     _update_MI_stances(decision)
-
+    logger.LOGGER.info("Decision metrics update completed.")
 
 def _update_regular_stances(decision):
     """ Updates the various stances within decision. In particular, this
@@ -53,22 +54,24 @@ def _update_regular_stances(decision):
     Arguments:
         decision: The decision object to update
     """
+    logger.LOGGER.info("Updating regular stances...")
     fors = decision.for_stances
     agns = decision.agn_stances
     bill = PymongoDB.get_db().find_one(db_constants.BILLS,
         queries.bill_query(decision.bill))
 
-
+    logger.LOGGER.info("Updating norms...")
     decision.for_norms = stance_analyze.collect_normative_stances(fors)
     decision.agn_norms = stance_analyze.collect_normative_stances(agns)
 
     if bill is None:
         logger.LOGGER.error("Decision bill not found.")
     else:
+        logger.LOGGER.info("Updating bill norms...")
         decision.for_bill_norms = stance_analyze.collect_normative_stances(bill.stances_for)
         decision.agn_bill_norms = stance_analyze.collect_normative_stances(bill.stances_agn)
 
-
+    logger.LOGGER.info("Updating group stances...")
     decision.groups_for = util.collect_group_stances(fors)
     decision.groups_agn = util.collect_group_stances(agns)
 
@@ -82,17 +85,23 @@ def _update_regular_stances(decision):
     agn_intersect = util.intersection(decision.groups_agn,
         decision.groups_for, eq_fun)
     decision.split_group = for_intersect + agn_intersect
+    if decision.split_group:
+        logger.LOGGER.info("Split group detected.")
+
 
     for_bills = util.collect_bill_stances(fors)
     agn_bills = util.collect_bill_stances(agns)
     if for_bills and agn_bills:
+        logger.LOGGER.info("Split record detected.")
         decision.split_record = for_bills + agn_bills
 
     for_credo = util.collect_credo_stances(fors)
     agn_credo = util.collect_credo_stances(agns)
     if for_credo and agn_credo:
+        logger.LOGGER.info("Split credo detected.")
         decision.split_credo = for_credo + agn_credo
 
+    logger.LOGGER.info("Regular stances update complete.")
 
 def _update_MI_stances(decision):
     """This function takes the stances for and against the decision and checks
@@ -104,12 +113,13 @@ def _update_MI_stances(decision):
     Arguments:
         decision: the decision object to update.        
     """
+    logger.LOGGER.info("Updating MI stances...")
     decision.MI_stance = _MI_stances(decision)
     decision.MI_group = _MI_stances(decision, db_constants.GROUPS)
     decision.MI_credo = _MI_stances(decision, db_constants.MEMBERS)
     decision.MI_record = _MI_stances(decision, db_constants.BILLS)
     decision.MI_norm = _compare_stances(decision.for_norms, decision.agn_norms)
-
+    logger.LOGGER.info("MI stances update complete.")
 
 def _MI_stances(decision, db_source=None):
     """Updates a specific MI_stance such as MI_stance or MI_group. It take the
@@ -126,6 +136,7 @@ def _MI_stances(decision, db_source=None):
             
             [[FOR|AGN], List_Of_Compelling_Stances_From_Winning_Side]
     """
+    logger.LOGGER.info("Updating MI stances from source db %s" % db_source)
     fors = decision.for_stances
     agns = decision.agn_stances
 
@@ -186,11 +197,14 @@ def _compare_stances(fors, agns):
             result = ResultData()
             result.outcome = outcomes.FOR
             result.data = util.remove_less_important_stances(fors[index:])
+            logger.LOGGER.info("MI stance is FOR the bill.")
             return result
         if an_agn.sort_key > a_for.sort_key:
             result = ResultData()
             result.outcome = outcomes.AGN
             result.data = util.remove_less_important_stances(agns[index:])
+            logger.LOGGER.info("MI stance is AGN the bill.")
             return result
+    logger.LOGGER.info("MI stance is neutral on the bill.")
     return []
 
